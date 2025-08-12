ce8bef8c3ae29b5205834808e75fa4a83840a7cb
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-12 15:28:21 +0530

    Refactor code structure for improved readability and maintainability

---
+++ backend/src/routes/food.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const foodController = require("../controllers/food.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+const router = express.Router();
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+
+/* POST /api/food/ [protected]*/
+router.post('/',
+    authMiddleware.authFoodPartnerMiddleware,
+    upload.single("mama"),
+    foodController.createFood)
+
+module.exports = router
